562ab54105928667f972ce80f6297eb853d7870d
goweii <goweii@163.com>
2019-08-18 13:03:11 +0800

    更新DEMO

--- anylayer/src/main/java/per/goweii/anylayer/AnyLayer.java
+++ anylayer/src/main/java/per/goweii/anylayer/AnyLayer.java
@@ -15,7 +15,6 @@ import per.goweii.burred.Blurred;
 public final class AnyLayer {
 
     public static void initBlurred(Context context) {
-        Utils.requireNonNull(context, "context == null");
         Blurred.init(context);
     }
 
@@ -23,44 +22,22 @@ public final class AnyLayer {
         Blurred.recycle();
     }
 
-    /**
-     * 此时不需要APP存在Activity实例
-     * 会新启动一个Activity并向根布局添加一个浮层
-     */
     public static void dialog(LayerActivity.OnLayerCreatedCallback callback) {
         LayerActivity.start(ActivityHolder.getApplication(), callback);
     }
 
-    /**
-     * 向窗口根布局添加一个浮层
-     */
     public static DialogLayer dialog() {
         return new DialogLayer(ActivityHolder.getCurrentActivity());
     }
 
-    /**
-     * 向窗口根布局添加一个浮层
-     */
     public static DialogLayer dialog(Class<Activity> clazz) {
-        Utils.requireNonNull(clazz, "clazz == null");
         return new DialogLayer(ActivityHolder.getActivity(clazz));
     }
 
-    /**
-     * 向窗口根布局添加一个浮层
-     *
-     * @param context 上下文，不能是ApplicationContext
-     */
     public static DialogLayer dialog(Context context) {
         return new DialogLayer(context);
     }
 
-    /**
-     * 向窗口根布局添加一个浮层，且位置参照targetView
-     * 及类似PopupWindow效果
-     *
-     * @param targetView 位置参照View
-     */
     public static PopupLayer popup(View targetView) {
         return new PopupLayer(targetView);
     }
