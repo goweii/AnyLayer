cd253e7c90d66a3abcf7a28dd5f70370b01ed7c6
goweii <goweii@163.com>
2020-08-15 22:31:21 +0800

    修复代码错误

--- anylayer-ext/src/main/java/per/goweii/anylayer/ext/NullAnimatorCreator.java
+++ anylayer-ext/src/main/java/per/goweii/anylayer/ext/NullAnimatorCreator.java
@@ -1,7 +1,26 @@
 package per.goweii.anylayer.ext;
 
-public class NullAnimatorCreator extends CommonAnimatorCreator {
-    public NullAnimatorCreator() {
-        super(AnimatorStyle.NONE);
+import android.animation.Animator;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import per.goweii.anylayer.Layer;
+
+/**
+ * @author CuiZhen
+ */
+public class NullAnimatorCreator implements Layer.AnimatorCreator {
+    @Nullable
+    @Override
+    public Animator createInAnimator(@NonNull View target) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Animator createOutAnimator(@NonNull View target) {
+        return null;
     }
 }
